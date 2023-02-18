37f50ce0cc2aedbc49abb4d83f18888378c59eeb
showmitra68 <123936677+showmitra68@users.noreply.github.com>
2023-02-18 15:48:41 +0600

    Add files via upload

---
+++ Lab-2_PHP/largest.php
@@ -0,0 +1,15 @@
+<html>
+    <?php
+    $a=10;
+    $b=15;
+    $c=12;
+
+    if($a>$b && $a>$c){
+        echo "a is the largest number";
+    }elseif($b>$a && $b>$c){
+        echo "b is the largest number";
+    }else{
+        echo "c is the largest number";
+    }
+    ?>
+</html>
---
+++ Lab-2_PHP/odd.php
@@ -0,0 +1,11 @@
+<html>
+    <?php
+    $a=18;
+    if($a%2==0){
+        echo "It's an even number"."<br>";
+    }
+    else{
+        echo"It's an odd number";
+    }
+    ?>
+</html>
